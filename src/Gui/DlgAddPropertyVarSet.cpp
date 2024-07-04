5e50626db453873f97965dd5c5a25a71b881b153
Adrian Insaurralde Avalos <adrianinsaval@gmail.com>
2024-07-04 15:10:11 -0400

    Gui: Add font data to VarSet dialog

--- src/Gui/DlgAddPropertyVarSet.cpp
+++ src/Gui/DlgAddPropertyVarSet.cpp
@@ -223,9 +223,12 @@ static PropertyEditor::PropertyItem *createPropertyItem(App::Property *prop)
     return item;
 }
 
-void DlgAddPropertyVarSet::addEditor(PropertyEditor::PropertyItem* propertyItem, std::string& /*type*/)
+void DlgAddPropertyVarSet::addEditor(PropertyEditor::PropertyItem* propertyItem, std::string& type)
 {
     editor.reset(propertyItem->createEditor(this, this, SLOT(valueChanged())));
+    if (type == "App::PropertyFont") {
+        propertyItem->setEditorData(editor.get(), QVariant());
+    }
     editor->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
     editor->setObjectName(QString::fromUtf8("editor"));
     auto formLayout = qobject_cast<QFormLayout*>(layout());
@@ -279,7 +282,7 @@ void DlgAddPropertyVarSet::createProperty(std::string& name, std::string& group)
     if (propertyItem && isTypeWithEditor(type)) {
         propertyItem->setPropertyData({prop});
         propertyItem->bind(*objectIdentifier);
-             addEditor(propertyItem.get(), type);
+        addEditor(propertyItem.get(), type);
     }
 
     setOkEnabled(true);
